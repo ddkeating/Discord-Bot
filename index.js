3b2148c63d94c0cc7e956756c0cb6f2be5ad43dc
ddkeating <73508313+ddkeating@users.noreply.github.com>
2023-11-15 13:20:28 +1300

    Improved warzone embeds.

--- index.js
+++ index.js
@@ -1,99 +1,127 @@
-const dotenv = require('dotenv').config();
-const { Client, Collection, Events, GatewayIntentBits, User, ButtonInteraction, Collector, ButtonComponent } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-const { readData, handleCaptureDetails } = require('./modules/functions');
+const dotenv = require("dotenv").config();
+const {
+	Client,
+	Collection,
+	Events,
+	GatewayIntentBits,
+	User,
+	ButtonInteraction,
+	Collector,
+	ButtonComponent,
+} = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { readData, handleCaptureDetails } = require("./modules/functions");
 
 const TOKEN = process.env.DISCORD_TOKEN; // Discord Bot Token
-const REQUIRED_ROLE = '1169773205100171294'; // Crucio Member role
-const STOP_BUTTON_ID = 'Stop-Capturing';
+const REQUIRED_ROLE = "1169773205100171294"; // Crucio Member role
+const STOP_BUTTON_ID = "Stop-Capturing";
 
 // Defined bot intents as per bot access requirements.
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-    ],
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.MessageContent,
+	],
 });
 
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 // Handles slash command execution.
 for (const folder of commandFolders) {
-    const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const filePath = path.join(commandsPath, file);
-        const command = require(filePath);
-        if ('data' in command && 'execute' in command) {
-            client.commands.set(command.data.name, command);
-        } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-        }
-    }
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith(".js"));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ("data" in command && "execute" in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
 }
 
 // Assigns bot in using bot auth token.
 client.login(TOKEN);
 
-
 client.on(Events.InteractionCreate, async (interaction) => {
-    const isButtonInteraction = interaction.isButton();
-    const isChatInputCommand = interaction.isCommand();
+	const isButtonInteraction = interaction.isButton();
+	const isChatInputCommand = interaction.isCommand();
 
-    // If interaction is button, will handle button based on button id.
-    if (isButtonInteraction && interaction.customId === STOP_BUTTON_ID) {
-        // Ensure that the interaction is of type ButtonInteraction
-        if (interaction instanceof ButtonInteraction) {
-            const originalInteraction = interaction.message.interaction;
+	// If interaction is button, will handle button based on button id.
+	if (isButtonInteraction && interaction.customId === STOP_BUTTON_ID) {
+		// Ensure that the interaction is of type ButtonInteraction
+		if (interaction instanceof ButtonInteraction) {
+			const originalInteraction = interaction.message.interaction;
 
-            if (originalInteraction.user instanceof User && interaction.user instanceof User) {
-                // Compare the user who clicked the button with the user who originally created the interaction
-                if (originalInteraction.user.id === interaction.user.id) {
-                    const warzoneLog = readData(interaction);
-                    handleCaptureDetails(interaction, warzoneLog);
-                    
+			if (
+				originalInteraction.user instanceof User &&
+				interaction.user instanceof User
+			) {
+				// Compare the user who clicked the button with the user who originally created the interaction
+				if (originalInteraction.user.id === interaction.user.id) {
+					const warzoneLog = readData(interaction);
+					handleCaptureDetails(interaction, warzoneLog);
+				} else {
+					// Prevents other users from using the embedded.
+					interaction.reply({
+						content:
+							"You can't click this button because you didn't create the original interaction.",
+						ephemeral: true,
+					});
+				}
+			}
+		}
+		return;
+	}
+	if (isChatInputCommand) {
+		const command = interaction.client.commands.get(interaction.commandName);
 
-                } else { // Prevents other users from using the embedded.
-                    interaction.reply({ content: "You can't click this button because you didn't create the original interaction.", ephemeral: true });
-                }
-            }
-        }
-        return;
-    }
-    if (isChatInputCommand) {
-        const command = interaction.client.commands.get(interaction.commandName);
+		if (!command) {
+			console.error(`No command matching ${interaction.commandName}`);
+			return;
+		}
 
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName}`);
-            return;
-        }
+		// Check if the interaction is in a guild context
+		if (interaction.guild) {
+			const member = interaction.guild.members.cache.get(interaction.user.id);
+			const requiredRole = interaction.guild.roles.cache.find(
+				(role) => role.id === REQUIRED_ROLE
+			);
 
-        // Check if the interaction is in a guild context
-        if (interaction.guild) {
-            const member = interaction.guild.members.cache.get(interaction.user.id);
-            const requiredRole = interaction.guild.roles.cache.find(role => role.id === REQUIRED_ROLE);
-
-            if (!member.roles.cache.has(requiredRole.id)) {
-                interaction.reply('You do not have the required permissions to use this command.');
-                return;
-            }
-        }
-        // Will attempt to execute code     
-        try { 
-            await command.execute(interaction);
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied || interaction.deferred) {
-                await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-            } else {
-                await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-            }
-        }
-    }
-})
+			if (!member.roles.cache.has(requiredRole.id)) {
+				interaction.reply(
+					"You do not have the required permissions to use this command."
+				);
+				return;
+			}
+		}
+		// Will attempt to execute code
+		try {
+			await command.execute(interaction);
+		} catch (error) {
+			console.error(error);
+			if (interaction.replied || interaction.deferred) {
+				await interaction.followUp({
+					content: "There was an error while executing this command!",
+					ephemeral: true,
+				});
+			} else {
+				await interaction.reply({
+					content: "There was an error while executing this command!",
+					ephemeral: true,
+				});
+			}
+		}
+	}
+});
--- modules/functions.js
+++ modules/functions.js
@@ -1,159 +1,182 @@
-// This controls the functions for all interactions, commands, and responses. 
+// This controls the functions for all interactions, commands, and responses.
 
-const axios = require('axios');
-const { cartelEmbeded, warzoneEmbedded } = require('./embedMessage.js');
-const fs = require('fs');
-
+const axios = require("axios");
+const { cartelEmbeded, warzoneEmbedded } = require("./embedMessage.js");
+const fs = require("fs");
 
 async function updateCartels(message) {
-    try {
-        const response = await axios.get('https://stats.olympus-entertainment.com/api/v3.0/cartels/', {
-            headers: {
-                'accept': 'application/json',
-                'Authorization': process.env.GANG_TOKEN
-            }
-        });
-        const responseData = response.data;
-        const tempArray = [];
+	try {
+		const response = await axios.get(
+			"https://stats.olympus-entertainment.com/api/v3.0/cartels/",
+			{
+				headers: {
+					accept: "application/json",
+					Authorization: process.env.GANG_TOKEN,
+				},
+			}
+		);
+		const responseData = response.data;
+		const tempArray = [];
 
-        message.reply('Warzone Caps...')
-        // Process and send data in custom embeds
-        responseData.forEach(entry => {
-            const { server, name, full_name, gang_name, progress } = entry;
-            const imagePath = {
-                "arms": 'https://i.imgur.com/TwArxSw.jpg',
-                "moonshine": 'https://i.imgur.com/Q4WWcg3.jpg',
-                "meth": 'https://i.imgur.com/8akwyzN.jpg'
-            }; 
+		message.reply("Warzone Caps...");
+		// Process and send data in custom embeds
+		responseData.forEach((entry) => {
+			const { server, name, full_name, gang_name, progress } = entry;
+			const imagePath = {
+				arms: "https://i.imgur.com/TwArxSw.jpg",
+				moonshine: "https://i.imgur.com/Q4WWcg3.jpg",
+				meth: "https://i.imgur.com/8akwyzN.jpg",
+			};
 
-
-            if (server === 1) {
-                const imageURL = imagePath[name.toLowerCase()];
-                const tempVal = cartelEmbeded({ fullName: full_name, gangName: gang_name, capProgress: progress, imagePath: imageURL });
-                tempArray.push(tempVal)
-                message.channel.send({ embeds: [tempVal] });
-            }
-
-        });
-    } catch (error) {
-        console.error(`An error occurred while fetching data: ${error}`);
-        message.channel.send('An error occurred while fetching data.');
-    }
+			if (server === 1) {
+				const imageURL = imagePath[name.toLowerCase()];
+				const tempVal = cartelEmbeded({
+					fullName: full_name,
+					gangName: gang_name,
+					capProgress: progress,
+					imagePath: imageURL,
+				});
+				tempArray.push(tempVal);
+				message.channel.send({ embeds: [tempVal] });
+			}
+		});
+	} catch (error) {
+		console.error(`An error occurred while fetching data: ${error}`);
+		message.channel.send("An error occurred while fetching data.");
+	}
 }
 
 async function shedInventory(message) {
-    try {
-        const response = await axios.get('https://stats.olympus-entertainment.com/api/v3.0/shed-inventory/1207/', {
-            headers: {
-                'accept': 'application/json',
-                'Authorization': process.env.GANG_TOKEN
-            }
-        });
+	try {
+		const response = await axios.get(
+			"https://stats.olympus-entertainment.com/api/v3.0/shed-inventory/1207/",
+			{
+				headers: {
+					accept: "application/json",
+					Authorization: process.env.GANG_TOKEN,
+				},
+			}
+		);
 
-        // Convert the response data to a formatted JSON string
-        const responseDataString = JSON.stringify(response.data, null, 2);
+		// Convert the response data to a formatted JSON string
+		const responseDataString = JSON.stringify(response.data, null, 2);
 
-        // Send the formatted JSON string to the Discord channel
-        message.reply('Shed Inventory:\n```json\n' + responseDataString + '\n```');
-    } catch (error) {
-        console.error(`Caught unexpected error while retrieving shed inventory, ${error}`);
-    }
+		// Send the formatted JSON string to the Discord channel
+		message.reply("Shed Inventory:\n```json\n" + responseDataString + "\n```");
+	} catch (error) {
+		console.error(
+			`Caught unexpected error while retrieving shed inventory, ${error}`
+		);
+	}
 }
 
 async function startTimer(interaction) {
-    try {
-        const currentTime = Date.now()
-        const response = await axios.get('https://stats.olympus-entertainment.com/api/v3.0/gangs/47740/', {
-            headers: {
-                'accept': 'application/json',
-                'Authorization': process.env.GANG_TOKEN
-            }
-        });
-        const formattedMoney = response.data.bank.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-        const tempVal = warzoneEmbedded({ time: Math.floor(currentTime), currentGangFunds: formattedMoney, user: interaction.member });
-        await interaction.reply(tempVal);
+	try {
+		const currentTime = Date.now();
+		const response = await axios.get(
+			"https://stats.olympus-entertainment.com/api/v3.0/gangs/47740/",
+			{
+				headers: {
+					accept: "application/json",
+					Authorization: process.env.GANG_TOKEN,
+				},
+			}
+		);
+		const formattedMoney = response.data.bank
+			.toString()
+			.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		const tempVal = warzoneEmbedded({
+			time: Math.floor(currentTime),
+			currentGangFunds: formattedMoney,
+			user: interaction.member,
+		});
+		await interaction.reply(tempVal);
 
-        storeData(interaction, formattedMoney, currentTime)
-
-
-        
-    } catch (error) {
-        console.error(error)
-        interaction.reply(`There was an unexpected error: ${error}`)
-    }
-
-};
-
+		storeData(interaction, formattedMoney, currentTime);
+	} catch (error) {
+		console.error(error);
+		interaction.reply(`There was an unexpected error: ${error}`);
+	}
+}
 
 async function storeData(interaction, money, time) {
-    try {
-        const player = interaction.member.displayName;
-        
-        // Load user data from the JSON file
-        let userData = {};
+	try {
+		const player = interaction.member.id;
 
-        try {
-            const data = fs.readFileSync('warzonelogs.json', 'utf-8');
-            userData = JSON.parse(data);
-        } catch (err) {
-            console.error('Error reading warzonelogs.json:', err);
-        }
+		// Load user data from the JSON file
+		let userData = {};
 
-        // Define the data you want to store
-        const currentGangFunds = money;/* Get the current gang funds for the user */
+		try {
+			const data = fs.readFileSync("warzonelogs.json", "utf-8");
+			userData = JSON.parse(data);
+		} catch (err) {
+			console.error("Error reading warzonelogs.json:", err);
+		}
 
-        // Store user-specific data in the JSON object
-        userData[player] = { currentGangFunds, playerName: player, currentTime: new Date(time).toLocaleTimeString() };
+		// Define the data you want to store
+		const currentGangFunds =
+			money; /* Get the current gang funds for the user */
 
-        // Save the updated user data to the JSON file
-        fs.writeFileSync('warzonelogs.json', JSON.stringify(userData, null, 2));
-    } catch (error) {
-        console.log(error);
-    }
+		// Store user-specific data in the JSON object
+		userData[player] = {
+			currentGangFunds,
+			playerName: player,
+			currentTime: new Date(time).toLocaleTimeString(),
+		};
+
+		// Save the updated user data to the JSON file
+		fs.writeFileSync("warzonelogs.json", JSON.stringify(userData, null, 2));
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 async function readData(interaction) {
-    const playerName = interaction.member.displayName;
-    try {
-        const data = JSON.parse(fs.readFileSync('warzonelogs.json'));
+	const playerName = interaction.member.displayName;
+	try {
+		const data = JSON.parse(fs.readFileSync("warzonelogs.json"));
 
-        // Check if the playerName exists in the JSON data
-        if (data[playerName]) {
-            const foundList = data[playerName]
-            delete data[playerName]
-            return foundList;
-        } else {
-          return null; // Return null if the playerName doesn't exist
-        }
-    } catch (error) {
-        console.log(error)
-    }
+		// Check if the playerName exists in the JSON data
+		if (data[playerName]) {
+			const foundList = data[playerName];
+			delete data[playerName];
+			return foundList;
+		} else {
+			return null; // Return null if the playerName doesn't exist
+		}
+	} catch (error) {
+		console.log(error);
+	}
 }
-
 
 async function handleCaptureDetails(interaction, data) {
-    const dataObject = await data;
-    const { currentGangFunds, playerName, currentTime } = dataObject;
-    
-    const response = await axios.get('https://stats.olympus-entertainment.com/api/v3.0/gangs/47740/', {
-        headers: {
-            'accept': 'application/json',
-            'Authorization': process.env.GANG_TOKEN
-        }
-    });
-    
-    let newGangBank = response.data.bank;
-    let reformattedMoney = currentGangFunds.replace(/,/g, '');
-    const diff = newGangBank - reformattedMoney;
+	const dataObject = await data;
+	const { currentGangFunds, playerName, currentTime } = dataObject;
 
-    if (isNaN(diff) || diff == 0) {
-        interaction.reply(`Member ${playerName} made no profit from holding capture points.`);
-    } else {
-        interaction.reply(`Member ${playerName} made $${diff.toLocaleString()} from holding capture points.`);
-    }
+	const response = await axios.get(
+		"https://stats.olympus-entertainment.com/api/v3.0/gangs/47740/",
+		{
+			headers: {
+				accept: "application/json",
+				Authorization: process.env.GANG_TOKEN,
+			},
+		}
+	);
+
+	let newGangBank = response.data.bank;
+	let reformattedMoney = currentGangFunds.replace(/,/g, "");
+	const diff = newGangBank - reformattedMoney;
+
+	if (isNaN(diff) || diff <= 0) {
+		interaction.reply(
+			`Member @${playerName} made no profit from holding capture points.`
+		);
+	} else {
+		interaction.reply(
+			`Member @${playerName} made $${diff.toLocaleString()} from holding capture points.`
+		);
+	}
 }
-
-
 
 exports.updateCartels = updateCartels;
 exports.shedInventory = shedInventory;
